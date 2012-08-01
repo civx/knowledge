80cf0f0593a7cae3d627a2be20f7216dcc4e076d
Nathaniel Case <Qalthos@gmail.com>
2012-08-01 15:53:44 -0400

    Pulling knowledge out of civx to its own repo.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+        name="knowledge",
+        version="0.1",
+        author="Luke Macken",
+        author_email="lmacken@redhat.com",
+        install_requires=['sqlalchemy'],
+        packages=find_packages(exclude=['ez_setup']),
+        zip_safe=True,
+      )
