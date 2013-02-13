4860debc85d1dd86f09af7246b41b069c458eb0e
Ralph Bean <rbean@redhat.com>
2013-02-13 16:10:19 -0500

    First tests!

--- setup.py
+++ setup.py
@@ -1,11 +1,16 @@
 from setuptools import setup, find_packages
 
+import multiprocessing
+import logging
+
 setup(
         name="knowledge",
         version="0.2",
         author="Luke Macken",
         author_email="lmacken@redhat.com",
         install_requires=['sqlalchemy'],
+        tests_require=['nose'],
+        test_suite='nose.collector',
         packages=find_packages(exclude=['ez_setup']),
         zip_safe=True,
       )
