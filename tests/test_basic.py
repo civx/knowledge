842cf7a636bafb8a3c144091b6d6b0a142cd54cf
Ralph Bean <rbean@redhat.com>
2013-06-19 00:12:07 -0400

    Global DBSession objects are a bad habit we learned from TG2.
    Here I remove it.  We found it was giving us problems when we weren't
    super careful in flask land.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,7 +4,6 @@ import os
 
 from sqlalchemy import create_engine
 from knowledge.model import setup_knowledge, Entity
-from knowledge.model import DBSession
 
 # TODO -- make this an in-memory database, not in /tmp
 filename = '/tmp/testing-knowledge-db.db'
@@ -12,13 +11,14 @@ filename = '/tmp/testing-knowledge-db.db'
 class TestBasics(unittest.TestCase):
     def setUp(self):
         uri = "sqlite:///%s" % filename
-        setup_knowledge(uri)
+        self.session = setup_knowledge(uri)
         #engine = create_engine('sqlite:///knowledge.db')
         #init_model(engine)
         #metadata.create_all(engine)
 
     def tearDown(self):
         os.remove(filename)
+        self.session.close()
 
     def test_basic_one(self):
         """ Basic usage. """
@@ -28,22 +28,22 @@ class TestBasics(unittest.TestCase):
     def test_basic_two(self):
         apple = Entity('apple')
         eq_(apple.name, 'apple')
-        DBSession.commit()
+        self.session.commit()
         # Did we survive?
 
     def test_associating_facts_unicode_by_key(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple['foo'], u'bar')
 
     def test_associating_facts_unicode_by_attr(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
-        apple['foo'] = u'bar'
-        eq_(apple.foo, 'bar')
+        self.session.add(apple)
+        self.session.commit()
+        apple[u'foo'] = u'bar'
+        eq_(apple.foo, u'bar')
 
 if __name__ == '__main__':
     unittest.main()
