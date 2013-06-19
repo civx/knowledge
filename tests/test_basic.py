939d7be400a2e136b142e38c3124a7db8707ec15
Ralph Bean <rbean@redhat.com>
2013-06-19 00:38:43 -0400

    Remove that global DBSession.
    Its a bad habit that we picked up from TG2.

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -4,56 +4,51 @@ import os
 
 from sqlalchemy import create_engine
 
-from knowledge.model import init_model, metadata, DBSession, Entity
+from knowledge.model import init_model, metadata, Entity, setup_knowledge
 
 
 class TestBasics(unittest.TestCase):
     def setUp(self):
-        engine = create_engine('sqlite:///:memory:')
-        init_model(engine)
-        metadata.create_all(engine)
-
-    def tearDown(self):
-        DBSession.remove()
+        self.session = setup_knowledge('sqlite:///:memory:')
 
     def test_basic_one(self):
         """ Basic usage. """
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         eq_(apple.name, 'apple')
 
     def test_associating_facts_unicode_by_key(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple['foo'], 'bar')
 
     def test_associating_facts_unicode_by_attr(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple.foo, 'bar')
 
     def test_associating_facts_unicode_by_get(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple.get('foo'), 'bar')
 
     def test_associating_facts_get_default(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple.get('baz'), None)
 
     def test_associating_facts_get_custom_default(self):
         apple = Entity('apple')
-        DBSession.add(apple)
-        DBSession.commit()
+        self.session.add(apple)
+        self.session.commit()
         apple['foo'] = u'bar'
         eq_(apple.get('baz', 'zomg'), 'zomg')
